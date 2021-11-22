f75b2c49224a2ff970c435e583a43f8759d89abe
MariyamTanveer <63596777+MariyamTanveer@users.noreply.github.com>
2021-11-22 13:30:17 +0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Writing our first tests with Jest</h1>
+<div>
+Write the following two tests for the <strong>isEmail</strong> function
+<ul>
+<li>The function should return <strong>true</strong> when a valid email is passed into it.</li>
+<li>The function should return <strong>false</strong> when an invalid email is passed into it.</li>
+</ul>
+</div>
+`;
